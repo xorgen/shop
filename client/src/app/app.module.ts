fadd8cc959dd99d8e70e4fd9a70b8133905a0036
Claire <claire@empower.social>
2023-02-04 21:04:26 +0100

    npx install & prettier config file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, NavigationComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- server/app.js
+++ server/app.js
@@ -3,16 +3,14 @@ const app = express();
 
 const port = 5000;
 
-app.get("/", (req, res) => {
-
-    res.send("Welcome to Express");
-  
-  });
+app.get('/', (req, res) => {
+	res.send('Welcome to Express');
+});
 
 app.listen(port, (err) => {
-    if (err) {
-      console.error('Something bad happened');
-    } else {
-      console.log(`Server is listening on ${port}`);
-    }
-  });
+	if (err) {
+		console.error('Something bad happened');
+	} else {
+		console.log(`Server is listening on ${port}`);
+	}
+});
