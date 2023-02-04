f2fe290bfc6acde2ca6ac140085067c6c08e3af9
Claire <claire@empower.social>
2023-02-04 04:29:52 +0100

    install express & nodemon

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+
+    res.send("Welcome to Express");
+  
+  });
+
+app.listen(port, (err) => {
+    if (err) {
+      console.error('Something bad happened');
+    } else {
+      console.log(`Server is listening on ${port}`);
+    }
+  });
